81b06ad3c708fcaf3d6215fb9affc9a8702c14c2
roxdavirox <roxdavirox@gmail.com>
2020-10-08 06:02:03 -0300

    hide form and load user data

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,9 @@
-import { getFormCookie } from '../services/cookieService';
+import { 
+  getFormCookie,
+  getUserName,
+  getUserPhone,
+  getUserEmail
+} from '../services/cookieService';
 import { loadEvents } from './events/formEvents';
 
 export const getJsonRequest = (props) => {
@@ -34,8 +39,50 @@ export const hideRequiredText = props => {
   return props
 }
 
+export const hideForm = (props) => {
+  console.log('[hideForm]');
+  const { setState } = props;
+  setState({ formVisible: false });
+  var formInputs = document.getElementsByClassName('orcamento-inputs');
+  for(var i = 0; i < orcamentoInputs.length; i++){
+    formInputs[i].style.display = 'none';
+  }
+  return props;
+}
+
+export function loadUserFormData(props) {
+  console.log('[loadUserFormData]');
+
+  var userName = getUserName();
+  if (!userName) {
+    console.log('user name not found');
+    return props;
+  }
+  var userPhone = getUserPhone();
+  if (!userPhone) {
+    console.log('user phone not found');
+    return props;
+  }
+  var userEmail = getUserEmail();
+  if (!userEmail){
+    console.log('user email not found');
+    return props;
+  }
+
+  document.getElementById('name').value = userName;
+  document.getElementById('phone').value = userPhone;
+  document.getElementById('email').value = userEmail;
+  return props;
+}
+
 export const loadFormEvents = (props) => {
   console.log('[loadFormEvents]');
+  var hasFormCookie = getFormCookie();
+  if (hasFormCookie && hasFormCookie === 'true') {
+    console.log('[loadFormEvents] formcookie', hasFormCookie);
+    hideForm(props);
+    loadUserFormData(props);
+  }
   loadEvents();
   return props;
 }
