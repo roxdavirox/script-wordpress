f734fe6c65542d62b044f78d7c13ea4500353e42
roxdavirox <roxdavirox@gmail.com>
2020-10-08 06:02:18 -0300

    user data cookie services

--- src/services/cookieService.js
+++ src/services/cookieService.js
@@ -1,3 +1,9 @@
 import { getCookie } from '../components/cookies';
 
 export const getFormCookie = () => getCookie('formCookie');
+
+export const getUserName = () => getCookie('user-name');
+
+export const getUserPhone = () => getCookie('user-phone');
+
+export const getUserEmail = () => getCookie('user-email');
