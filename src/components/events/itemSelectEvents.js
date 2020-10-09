ae9c6d1b9b3a07eac4b45585d59171928e33c9c0
roxdavirox <roxdavirox@gmail.com>
2020-10-09 00:48:04 -0300

    async event item select

--- src/components/events/itemSelectEvents.js
+++ src/components/events/itemSelectEvents.js
@@ -4,21 +4,15 @@ import { updateComponent } from '../select';
 export const onItemChange = props => async e => {
   e.preventDefault();
   console.log('[onItemChange]');
-  if (e.target.className !== 'item-select') {
-    console.log('evento chamado por outro elemento != item-select');
-    return;
-  }
-
-  // const unit = e.target.children[e.target.selectedIndex].getAttribute('_unit');
-  // const showUnitField = e.target.children[e.target.selectedIndex].getAttribute('_showunitfield');
   const { children, selectedIndex } = e.target;
   const child = children[selectedIndex];
-  const selectedItemId = child.getAttribute('id');
+  const selectedItemId = child.getAttribute('_itemid');
   const optionId = child.getAttribute('_optionid');
 
   const { html } = await updateComponent(props)(optionId, selectedItemId);
   const div = getDivByOptionId(optionId);
   div.innerHTML = html;
+  loadItemSelectEvents(props);
 }
 
 export const loadItemSelectEvents = props => {
@@ -27,6 +21,8 @@ export const loadItemSelectEvents = props => {
   if(!items) return;
 
   for(var i = 0; i < items.length; i++) {
-    items[i].onchange = e => onItemChange(props)(e);
+    items[i].onchange = async function onChange(e) {
+      await onItemChange(props)(e);
+    }
   }
 }
