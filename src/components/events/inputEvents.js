96e0a429c79599b0efbf4acdd60761d259c5822b
roxdavirox <roxdavirox@gmail.com>
2020-10-09 03:37:07 -0300

    feat input events

---
+++ src/components/events/inputEvents.js
@@ -0,0 +1,31 @@
+import { updatePrice } from "../form";
+
+export const loadInputEvents = props => {
+  console.log('[loadInputEvents]');
+  const inputQuantityList = document.querySelectorAll('[id=input-unit-quantity]');
+  console.log('[loadInputEvents]', inputQuantityList);
+
+  for(var i = 0; i < inputQuantityList.length; i++) {
+    inputQuantityList[i].onchange = function onChange(e) {
+      console.log('[loadInputEvents] onchange');
+      if (!e.target) return;
+      let itemId = e.target.getAttribute('_itemid');
+      const { setState, getState } = props;
+      const state = getState();
+      const { json } = state;
+      const newJson = { ...json, defaultItems: { 
+          ...json.defaultItems,
+          [itemId]: {
+            ...json.defaultItems,
+            quantity: e.target.value
+          }
+        }
+      };
+      setState({ json: newJson });
+      console.log('state', state);
+      // updateForm(e);
+      updatePrice(props);
+    }
+  }
+  return props;
+}
