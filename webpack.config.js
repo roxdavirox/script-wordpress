c1d437f35a6dbab271266d11e01abde2042b5271
roxdavirox <roxdavirox@gmail.com>
2020-10-09 03:11:03 -0300

    chore web pack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve('D:/www/mktp/site/', 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   }
 }
