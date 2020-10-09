0386213d48f51f1fad83d303cf8dbb362d51dfd1
roxdavirox <roxdavirox@gmail.com>
2020-10-09 03:36:52 -0300

    compose load events

--- src/components/events/formEvents.js
+++ src/components/events/formEvents.js
@@ -3,11 +3,18 @@ import { loadButtonEvents } from './buttonEvents';
 import { loadQuantityEvents } from './quantityEvents';
 import { loadSizeEvents } from './sizeEvents';
 import { loadItemSelectEvents } from './itemSelectEvents';
+import { loadInputEvents } from './inputEvents';
+import { compose } from '../../utils/compose';
 
 export const loadEvents = (props) => {
-  loadPhoneEvents(props);
-  loadButtonEvents(props);
-  loadQuantityEvents(props);
-  loadSizeEvents(props);
-  loadItemSelectEvents(props);
+  console.log('[loadEvents]');
+  compose([
+    loadPhoneEvents,
+    loadButtonEvents,
+    loadQuantityEvents,
+    loadSizeEvents,
+    loadItemSelectEvents,
+    loadInputEvents
+  ])(props);
+  return props;
 }
